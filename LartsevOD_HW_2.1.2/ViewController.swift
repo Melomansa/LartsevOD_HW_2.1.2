73e649639196cd964c006e4c61d2eed783c3f97c
1 <1>
2021-07-12 14:44:13 +0300

    create views and button

--- LartsevOD_HW_2.1.2/ViewController.swift
+++ LartsevOD_HW_2.1.2/ViewController.swift
@@ -9,11 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redLightView: UIView!
+    @IBOutlet weak var yellowLightView: UIView!
+    @IBOutlet weak var greenLightView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func startButton(_ sender: Any) {
+    }
+    
 }
 
