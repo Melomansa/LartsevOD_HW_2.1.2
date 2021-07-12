ea234f55468c9d4405bfc4acf248269bd0a13aa6
1 <1>
2021-07-12 11:01:53 +0300

    Initial Commit

---
+++ LartsevOD_HW_2.1.2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LartsevOD_HW_2.1.2
+//
+//  Created by Олег Ларцев on 12.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
